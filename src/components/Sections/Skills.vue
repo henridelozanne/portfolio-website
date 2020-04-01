65c72649c6d89507c0bb19ab38d4d9e1bde871a3
Henri Delozanne <hnr.delozanne@gmail.com>
2020-04-01 17:51:53 +0200

    Style skills page.

--- src/components/Sections/Skills.vue
+++ src/components/Sections/Skills.vue
@@ -1,10 +1,17 @@
 <template>
   <section>
     <section-title :title="'Technical skills'" :lightBackground="false"></section-title>
-    <div id="skills-ctn">
-      <skill-item v-for="skill in skills" :key="skill.name"
-                  :name="skill.name" :description="skill.description">
-      </skill-item>
+    <div class="skills-blocks-ctn">
+      <div class="left-block">
+        <skill-item v-for="skill in leftSkills" :key="skill.name"
+                  :name="skill.name" :description="skill.description" :position="'left'">
+        </skill-item>
+      </div>
+      <div class="right-block">
+        <skill-item v-for="skill in rightSkills" :key="skill.name"
+                  :name="skill.name" :description="skill.description" :position="'right'">
+        </skill-item>
+      </div>
     </div>
   </section>
 </template>
@@ -19,23 +26,31 @@ export default {
     'skill-item': SkillItem,
     'section-title': SectionTitle,
   },
+  computed: {
+    leftSkills() {
+      return this.skills.slice(0, 3);
+    },
+    rightSkills() {
+      return this.skills.slice(3);
+    },
+  },
   data() {
     return {
       skills: [{
         name: 'Vue.js',
-        description: 'and all related technologies : VueX, Nuxt.js, Vue Router, Vue Loader, Vue Devtools...',
-      }, {
-        name: 'Javascript',
-        description: 'daily practice of JS in ES6 notation',
+        description: 'and all related technologies : VueX, Nuxt.js, Vue Router, Vue Loader, Vue Devtools, Vuetify...',
       }, {
         name: 'HTML/SCSS',
         description: 'clean style declarations through SCSS, Tailwind enthusiast',
       }, {
-        name: 'Testing',
-        description: 'E2E testing with Cypress.io, unit testing with Jest',
-      }, {
         name: 'API',
         description: 'integration of external APIs to complete your website (Paypal API, Google Maps API, AM Charts…)',
+      }, {
+        name: 'Javascript',
+        description: 'daily practice of JS in ES6 notation',
+      }, {
+        name: 'Testing',
+        description: 'E2E testing with Cypress.io, unit testing with Jest',
       }, {
         name: 'UI Toolkits',
         description: 'Bootstrap, Material Design, Bulma, Element UI… everything needed for quick and clean styles applications',
@@ -48,5 +63,27 @@ export default {
 <style scoped lang="scss">
 section {
   background: theme('colors.custom-black');
+}
+
+.skills-blocks-ctn {
+  display: flex;
+  justify-content: center;
+  margin-top: 80px; /* FIXME: to be changed, won't be responsive like that */
+
+  div {
+    height: 700px;
+    width: 480px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .left-block {
+    margin-right: 70px;
+  }
+
+  .right-block {
+    margin-top: 50px;
+  }
 }
 </style>
--- src/components/SkillItem.vue
+++ src/components/SkillItem.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="skill-ctn">
-    <h3>{{name}}</h3>
-    <p>{{description}}</p>
+  <div class="skill-ctn"
+       :class="{'flex-rowreverse': position === 'right', 'border-visible': skillIsHovered}"
+       @mouseenter="toggleDescription" @mouseleave="toggleDescription">
+    <div class="content">
+      <h3 :class="{'text-primary': skillIsHovered}">{{name}}</h3>
+      <p v-show="skillIsHovered" class="description">{{description}}</p>
+    </div>
+    <!-- FIXME: Factor by using a dynamic src -->
+    <img v-if="name==='Vue.js'" src="../assets/skills-logos/vue-logo.png"
+         alt="vuejs-logo" :class="[{'hovered-img': skillIsHovered},
+         position === 'right' ? 'img-margin-right' : 'img-margin-left']">
+    <img v-else-if="name==='Javascript'" src="../assets/skills-logos/javascript-logo.png"
+         alt="javascript-logo" :class="[{'hovered-img': skillIsHovered},
+         position === 'right' ? 'img-margin-right' : 'img-margin-left']">
+    <img v-else-if="name==='HTML/SCSS'" src="../assets/skills-logos/html-logo.png"
+         alt="html-logo" :class="[{'hovered-img': skillIsHovered},
+         position === 'right' ? 'img-margin-right' : 'img-margin-left']">
+    <img v-else-if="name==='Testing'" src="../assets/skills-logos/cypress-logo.png"
+         alt="testing-logo" :class="[{'hovered-img': skillIsHovered},
+         position === 'right' ? 'img-margin-right' : 'img-margin-left']">
+    <img v-else-if="name==='API'" src="../assets/skills-logos/api-logo.png"
+         alt="api-logo" :class="[{'hovered-img': skillIsHovered},
+         position === 'right' ? 'img-margin-right' : 'img-margin-left']">
+    <img v-else-if="name==='UI Toolkits'" src="../assets/skills-logos/element-ui-logo.png"
+         alt="element-ui-logo" :class="[{'hovered-img': skillIsHovered},
+         position === 'right' ? 'img-margin-right' : 'img-margin-left']">
   </div>
 </template>
 
@@ -9,14 +32,71 @@
 export default {
   name: 'SkillItem',
   props: {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
+    name: { type: String, default: '' },
+    description: { type: String, default: '' },
+    position: { type: String, default: '' },
+  },
+  methods: {
+    toggleDescription() {
+      this.skillIsHovered = !this.skillIsHovered;
+    },
+  },
+  data() {
+    return {
+      skillIsHovered: false,
+    };
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .skill-ctn {
   color: theme('colors.custom-white');
+  display: flex;
+  flex-direction: row !important;
+  justify-content: flex-end !important;
+  align-items: center;
+  padding: 0 20px;
+
+  .content {
+
+    h3 {
+      font-weight: 600;
+      font-size: 28px;
+    }
+
+    .description {
+      font-size: 1.1em;
+      margin-top: 10px;
+    }
+  }
+
+  img {
+    filter: invert(46%) sepia(10%) saturate(2097%)
+    hue-rotate(135deg) brightness(94%) contrast(90%);
+    height: 100px !important;
+  }
+
+  .hovered-img {
+    filter: invert(96%) sepia(10%) saturate(1120%) hue-rotate(102deg)
+    brightness(96%) contrast(90%);
+  }
+
+  .img-margin-left {
+    margin-left: 50px;
+  }
+
+  .img-margin-right {
+    margin-right: 50px;
+  }
+}
+
+.border-visible {
+  border: 5px solid theme('colors.secondary');
+  border-radius: 15px;
+}
+
+.flex-rowreverse {
+  flex-direction: row-reverse !important;
 }
 </style>
